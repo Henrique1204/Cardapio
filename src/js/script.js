80c46d4a41c226c073fea8d9b075b67ac7e2633a
Paulo <pauloh16.pdr@gmail.com>
2021-02-04 15:37:32 -0300

    Adicionar script para horario de funcionamento

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
-console.log("Olá mundo!");
+const checarHorario = () => {
+    const spanHorario = document.querySelector('span[data-horario]');
+    const abre = document.querySelector('[data-horario="abre"]')?.innerText.replace(/\D/g, '');
+    const fecha = document.querySelector('[data-horario="fecha"]')?.innerText.replace(/\D/g, '');
+
+    if (spanHorario && abre && fecha) {
+        const hora = new Date().getHours();
+        const isAberto = hora >= abre && hora < fecha;
+
+        spanHorario.setAttribute('data-horario', isAberto ? 'aberto' : 'fechado');
+    }
+};
+
+checarHorario();
