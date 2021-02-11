acc2163454497f57227232542940655b32dda7d2
Paulo <pauloh16.pdr@gmail.com>
2021-02-11 03:15:05 -0300

    Implementar slide

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+import SlideNav from './slide.js';
+
 const checarHorario = () => {
     const spanHorario = document.querySelector('span[data-horario]');
     const abre = document.querySelector('[data-horario="abre"]')?.innerText.replace(/\D/g, '');
@@ -12,3 +14,25 @@ const checarHorario = () => {
 };
 
 checarHorario();
+
+const handleSlide = () => {
+    const { matches } = window.matchMedia('(max-width: 750px');
+    const wrapper = document.querySelector('[data-slide-wrapper]');
+    const slide = document.querySelector('[data-slide]');
+
+    if (matches) {
+        wrapper.classList.add('slide-wrapper');
+        slide.classList.add('slide');
+        slide.classList.remove('container');
+
+        const slideNav = new SlideNav('.slide', '.slide-wrapper');
+        slideNav.iniciar();
+    } else {
+        wrapper.classList.remove('slide-wrapper');
+        slide.classList.remove('slide');
+        slide.classList.add('container');
+    }
+};
+
+handleSlide();
+window.addEventListener('resize', handleSlide);
